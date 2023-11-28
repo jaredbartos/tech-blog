54cf11570837e9b77ec203e99c5c78370bfbd9a0
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-11-28 14:10:18 -0600

    Confirm initial server connection and homepage display

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const routes = require('./controllers')
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
@@ -30,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App is now listening at http://localhost:${PORT}`));
