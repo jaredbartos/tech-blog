db109eed304708b500ccada9d4a232749781a6ac
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-11-28 16:21:26 -0600

    Edit home route to correctly render homepage with seeded posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.render('homepage');
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+        }
+      ]
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', { posts });;
+  } catch (err) {
+    res.status(500).json(err);
+  };
+
 })
 
 module.exports = router;
