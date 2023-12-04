3196dbf3196f719c75d0785873ab34b7de7b508b
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-12-03 23:39:35 -0600

    Edit sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: 'localhost',
     dialect: 'mysql',
+    port: 3306,
   }
 );
 
